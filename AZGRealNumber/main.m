646c240b1d93d6180a2d4b8def63fd54d84ce2ff
安志钢 <AnZhg@AnZhg-MacBook-Pro.local>
2016-12-26 02:28:03 -0500

    Initial Commit

---
+++ AZGRealNumber/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  AZGRealNumber
+//
+//  Created by 安志钢 on 16-12-26.
+//  Copyright (c) 2016年 安志钢. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
